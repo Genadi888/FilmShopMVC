1d815cb9243c1d7cca0416c6473cf6cf71b9ebe5
Genadi888 <genadi.fidanov@abv.bg>
2025-03-22 20:02:56 +0200

    initial commit

--- FilmShopMVC/FilmShopMVC/Repositories/IHomeRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FilmShopMVC
-{
-    public interface IHomeRepository
-    {
-        Task<IEnumerable<Film>> GetFilms(string sTerm ="", int genreId = 0);
-        Task<IEnumerable<Genre>> Genres();
-    }
-}
